df80ec1706b04b51fd2467c564dbf4baa302c2e8
Port5053 <kamalabdellatifjena@gmail.com>
2016-02-09 15:08:44 +0100

    Rudimentärer Plan, wenigstens keine Fehler mehr?

--- Server/vp/static/js/upload.js
+++ Server/vp/static/js/upload.js
@@ -1,18 +1,8 @@
-/* Wenn der Browser JS nicht versteht, wird der Code nicht angezeigt sondern als Kommentar betrachtet. JS ignoriert den Kommentaranfang einfach. */
-
-/* Versuch, Drag and Drop mit jQuery/CSS ohne weitere Bibliotheken hinzubekommen */
-
-
-/* Browser machen sehr merkwürdige Sachen, 
-wenn man keine Default-Events verhindert
-
-evtl. highlighting der Box
-*/
 $(document).on(
     'dragover',
     function(e) {
-        e.stopPropagation();
         e.preventDefault();
+        e.stopPropagation();
         console.log('dragOver');
     }
 )
@@ -30,46 +20,31 @@ $(document).on(
     'drop',
     function(e){
         console.log('drop');
-        if(e.originalEvent.dataTransfer){
-            /* .files sind alle gedroppten Dateien */
-
-            /* Überprüfen ob >= eine Datei im Array ist*/
             if (e.originalEvent.dataTransfer.files.length) {
-                /* Defaultzeug*/
                 e.preventDefault();
                 e.stopPropagation();
-                /* Das hier sollte alles hochladen, ich hab kine Ahnung ob das funktioniert */
                 upload(e.originalEvent.dataTransfer.files);
             }   
         }
     }
 );
+
+function handleServerResponse(response) {
+    if (response.success) alert("Ja dat funktioniert");
+    else alert("Computer says no.");
+}
+
 function upload(files) {
-    var formData = new FormData($('form'));
-    
-    /* Tadaa, die Lösung des Problems: ganz viele einzelne 'file'-POST-vars (unglaublich, ich weiß) */
+    var formData = new FormData($('form')[0]); // besser?
     for (var i = 0; i < files.length; i++)
         formData.append('file', files[i]);
 
-    
     return $.ajax({
         url: path,
         type: 'POST',
         data: formData,
         processData: false,
         contentType: false,
-        success: function(response) {
-            // README
-            /*
-            nach 5h arbeit hab ich herausgefunden dasss der Server (richtig sick) JSON an die success-function übergeben kann.
-            jetzt kann ajax also den Server-Response handeln.
-            */
-
-            if (response.success) alert("Ja dat funktioniert");
-            //  ^^^^^^^^^^^^^^^^
-            //     die Flag          (kann auch genauere Info sein)
-            else alert("Computer says no.");
-        }
-
+        success: handleServerResponse
     });
 }
