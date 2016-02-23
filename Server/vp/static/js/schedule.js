61111a4ba2f2f1ccf85e94c848eeb7fb1b23be65
infoproject <bluelakefish@gmail.com>
2016-02-23 13:01:37 +0100

    funktionierende Transitions

--- Server/vp/static/js/schedule.js
+++ Server/vp/static/js/schedule.js
@@ -9,6 +9,7 @@ prepSlides = function(){
 
 max = function(i, j){ return Math.max(i, j); }
 min = function(i, j){ return Math.min(i, j); }
+var animationTime = 0.5;
 // "harte" Übergänge: aus- und einblenden statt blenden nach links und rechts;
 // funktioniert so aber immerhin
 toggleSlide = function(dir){
@@ -17,15 +18,19 @@ toggleSlide = function(dir){
   var nextSlide = dir=="left"?max(currentSlide-1, 0):min(currentSlide+1,slides.length-1);
   console.log("[DEBUG] SlideEvent // old:" + currentSlide + " new:" + nextSlide);
   // hier könnte man mit css3 transitions u. animations nen schönen Übergang bauen
+// currentSlide = aktuelle Folie, wird ausgebaut
+// nextSlide wird eingebaut
   if(nextSlide!=currentSlide){
     for(var i = 0; i < slides.length; i++) slides[i].style.display = "none";
     slides[nextSlide].style.display = slides[currentSlide].style.display = "block";
-    slides[currentSlide].style.animation = (nextSlide>currentSlide?"fade_left":"fade_right") + " 1s ease-out 0s 1";
-    slides[nextSlide].style.animation = (nextSlide<currentSlide?"from_left":"from_right") + " 1s ease-out 0s 1";
+    slides[currentSlide].style.animation = (nextSlide>currentSlide?"fade_left":"fade_right") + " " + animationTime + "s ease-in-out 0s 1";
+    slides[nextSlide].style.animation = (nextSlide<currentSlide?"from_left":"from_right") + " " + animationTime + "s ease-in-out 0s 1";
+    var tmpNext = nextSlide;
+    var tmpCurr = currentSlide;
     window.setTimeout(function(){
-      slides[nextSlide].style.display = "none";
-        slides[currentSlide].style.display = "none";
-    }, 1000);
+        slides[tmpNext].style.display = "block";
+        slides[tmpCurr].style.display = "none";
+    }, animationTime*1000);
   } else {
     slides[currentSlide].style.display = "none";
     slides[nextSlide].style.display = "block";
