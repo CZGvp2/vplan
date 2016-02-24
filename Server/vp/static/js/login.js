96c34aab973ba1618829be6bc84a21607401bef3
infoproject <bluelakefish@gmail.com>
2016-02-24 14:23:08 +0100

    test

--- Server/vp/static/js/login.js
+++ Server/vp/static/js/login.js
@@ -1,13 +1,15 @@
+// Make submit of the first form of the page available globally
 function submit() {
 	$('form').submit();
 }
+
+// Update keylisteners
 $(document).ready(function () {
 	$('#password').on('keyup keypress paste', function (event) {
 		if (event.keyCode == 13 && $('#password').val())
 			submit();
 
 		if ($('#password').val() || event.charCode) {
-			// #88A6FF - border-bottom, button bg
 			$('a#submit').css({'background-color':'#316495', 'cursor':'pointer'})
 			.off('click').on('click', submit);
 			$('#password').css('border-bottom-color', '#316495');
@@ -26,7 +28,9 @@ $(document).ready(function () {
 		$('#hash').val( CryptoJS.SHA512($('#password').val()) );
 		$('#password').val('');
 	});
-}); // das Semikolon hat gefehlt
+});
+
+// Check if jQuery is loading correctly
 (jQCheck = function(){
-	if(typeof $ === 'undefined') alert('(ERROR) jQuery konnte nicht geladen werden. Bitte wende dich an einen Server-Admin.')
+	if(typeof $ == 'undefined') alert('(ERROR) jQuery konnte nicht geladen werden. Bitte wende dich an einen Server-Admin.')
 })();
--- Server/vp/static/js/schedule.js
+++ Server/vp/static/js/schedule.js
@@ -1,65 +1,71 @@
-function filter(target) {
+// Die is schön
+filter = function(target) {
 	var events = $('.event');
-	if (target) {
-		for (var i=0; i < events.length; i++) {
+	if(target){
+		for(var i=0; i < events.length; i++){
 			var e = $(events[i]);
 			var targets = e.data().targets.split(' ');
 
 			if (targets.indexOf(target) < 0) e.hide();
 			else e.show();
 		}
-	} else events.show(); // target = null -> alles angezeigt
+	} else events.show();
 }
 
-var currentIndex = 0; // Irgendwie muss das dann am anfang der Index von der Slide von heute sein
+removeFilter = function(){
+  var events = $('.event');
+  for(var i = 0; i < events.length; i++){
+    $(events[i]).show();
+  }
+}
 
-// Alle Slides müssen ein vorgefertigtes Style-setup bekommen:
+var currentIndex = 0;
+
 prepSlides = function(){
   var slides = $('.slide');
   for(var i = 0; i < slides.length; i++)
-    slides[i].style.display = i==currentSlide?'block':'none';
-  $('#left').get(0).innerHTML = currentSlide>0?slides[currentSlide-1].children[0].innerHTML:'---';
-  $('#right').get(0).innerHTML = currentSlide<slides.length-1?slides[currentSlide+1].children[0].innerHTML:'---';
+    slides[i].style.display = i==currentIndex?'block':'none';
+  $('#left').get(0).innerHTML = currentIndex>0?slides[currentIndex-1].children[0].innerHTML:'---';
+  $('#right').get(0).innerHTML = currentIndex<slides.length-1?slides[currentIndex+1].children[0].innerHTML:'---';
 }
 
 max = function(i, j){ return Math.max(i, j); }
 min = function(i, j){ return Math.min(i, j); }
+
 var animationTime = 0.5;
-// 'harte' Übergänge: aus- und einblenden statt blenden nach links und rechts;
-// funktioniert so aber immerhin
+
 toggleSlide = function(dir){
   var slides = $(".slide");
   if(slides.length <= 1) return;
-  var nextSlide = dir=="left"?max(currentSlide-1, 0):min(currentSlide+1,slides.length-1);
-  console.log("[DEBUG] SlideEvent // old:" + currentSlide + " new:" + nextSlide);
-  // hier könnte man mit css3 transitions u. animations nen schönen Übergang bauen
-// currentSlide = aktuelle Folie, wird ausgebaut
-// nextSlide wird eingebaut
-  if(nextSlide!=currentSlide){
+  var nextSlide = dir=="left"?max(currentIndex-1, 0):min(currentIndex+1,slides.length-1);
+  if(nextSlide!=currentIndex){
     for(var i = 0; i < slides.length; i++) slides[i].style.display = "none";
-    slides[nextSlide].style.display = slides[currentSlide].style.display = "block";
-    slides[currentSlide].style.animation = (nextSlide>currentSlide?"fade_left":"fade_right") + " " + animationTime + "s ease-in-out 0s 1";
-    slides[nextSlide].style.animation = (nextSlide<currentSlide?"from_left":"from_right") + " " + animationTime + "s ease-in-out 0s 1";
+    slides[nextSlide].style.display = slides[currentIndex].style.display = "block";
+    slides[currentIndex].style.animation = (nextSlide>currentIndex?"fade_left":"fade_right") + " " + animationTime + "s ease-in-out 0s 1";
+    slides[nextSlide].style.animation = (nextSlide<currentIndex?"from_left":"from_right") + " " + animationTime + "s ease-in-out 0s 1";
     var tmpNext = nextSlide;
-    var tmpCurr = currentSlide;
+    var tmpCurr = currentIndex;
     window.setTimeout(function(){
         slides[tmpNext].style.display = "block";
         slides[tmpCurr].style.display = "none";
     }, animationTime*1000);
   } else {
-    slides[currentSlide].style.display = "none";
+    slides[currentIndex].style.display = "none";
     slides[nextSlide].style.display = "block";
   }
-  currentSlide = nextSlide;
-  $("#left").get(0).innerHTML = currentSlide>0?slides[currentSlide-1].children[0].innerHTML:"---";
-  $("#right").get(0).innerHTML = currentSlide<slides.length-1?slides[currentSlide+1].children[0].innerHTML:"---";
+  currentIndex = nextSlide;
+  $("#left").get(0).innerHTML = currentIndex>0?slides[currentIndex-1].children[0].innerHTML:"---";
+  $("#right").get(0).innerHTML = currentIndex<slides.length-1?slides[currentIndex+1].children[0].innerHTML:"---";
 }
+
 // EINE KOSTPROBE JQUERY
-toggleSlide2 = function(dir){
+//... die nicht funktioniert?!? schau mal auf die Tagesnamen an den Seiten,
+// wenn man toggleSlide in der .pt mit toggleSlide2 ersetzt hängt das updaten immer ne slide hinterher
+/*toggleSlide2 = function(dir){
   var slides = $('.slide');
   if(slides.length <= 1) return;
   var nextIndex = dir=='left'?max(currentIndex-1, 0):min(currentIndex+1,slides.length-1);
-  
+
   var currentSlide = $( slides[currentIndex] );
   var nextSlide = $( slides[nextIndex] );
 
@@ -85,15 +91,16 @@ toggleSlide2 = function(dir){
   var next = currentSlide.next('.slide');
   $("#left").text(prev.length ? prev.find('div.slide_title').text() : '---');
   $("#right").text(next.length ? next.find('div.slide_title').text() : '---');
-}
-
-/*
-document.body.onkeypress = function(e){
-  switch(e.which){
-    case 37: toggleSlide('left'); break;
-    case 38: toggleSlide('right'); break;
-    default: break;
-  }
 }*/
-// = $(document).ready(prepSlides);
+
+// Bist du jetzt glücklich?
+// Add keylistener to toggle slides via arrow keys
+$(document).on('keypress', function(evt){
+  switch(evt.keyCode){
+    case 37: toggleSlide('left'); break;
+    case 39: toggleSlide('right'); break;
+  }
+});
+
+// Call prepSlides() when DOM is ready
 $(prepSlides);
